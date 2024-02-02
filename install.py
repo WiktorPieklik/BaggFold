442bc177dd4bbed636235006558b5b098209dfb1
WiktorPieklik <wiktor.pieklik@icloud.com>
2024-02-02 23:09:46 +0100

    refactor: removed unnecessary dependencies

--- install.py
+++ install.py
@@ -5,22 +5,9 @@ dependencies = [
     'numpy',
     'pandas',
     'scikit-learn',
-    'matplotlib',
-    'jupyter',
-    'ghostml',
-    'ctgan',
-    'hmeasure',
     'imbalanced-learn',
     'scipy',
     'xgboost',
-    'tabulate',
-    'alive-progress',
-    'gym',
-    'keras-rl2',  # must appear before tensorflow dependencies
-    'tensorflow-macos==2.12.0',
-    'tensorflow-metal',
-    'pydot',
-    'simple_chalk',
 ]
 
 
